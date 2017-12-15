51927623d9ff7832e2032ebe563f656e285656a4
kothai03 <kothaimarch@gmail.com>
2017-12-15 15:15:48 +0530

    added basic clientside configuration

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 // Module dependency.
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 // Custom dependecy.
 const config = require('./config');
@@ -12,11 +13,12 @@ const { addStickyInfo, updateSticky } = require('./src/routes');
 const app = express();
 const router = express.Router();
 
+// Application middleware goes here.
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
-
 // parse application/json
 app.use(bodyParser.json());
+app.use('/', express.static(path.join(__dirname, 'client')));
 
 // routes.
 router.post('/add', addStickyInfo);
